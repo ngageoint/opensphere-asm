b359a5c23e94fd6856a2f49487cc589a1e60c7e8
William Wall <wwall@caci.com>
2019-09-12 12:31:32 -0600

    feat: add node support and meridian intersection support
    Adds support for loading in node (older node versions may need --experimental-modules). Also adds
    support for determining the coordinate at which a geodesic or rhumb line intersects a given
    meridian.

--- src/prefix.js
+++ src/prefix.js
@@ -1,39 +1,50 @@
 // get path to current script
-var script = document.currentScript;
-if (!script) {
-  var ss = document.getElementsByTagName('script');
-  var i=ss.length;
-  while(i--){
-    if(ss[i].src.indexOf('/os-load.js')>-1){
-      script=ss[i];
-      break;
+var isBrowser = function() {
+  try {
+    return this === window;
+  } catch (e) {
+  }
+  return false;
+};
+
+
+if (isBrowser()) {
+  var script = document.currentScript;
+  if (!script) {
+    var ss = document.getElementsByTagName('script');
+    var i=ss.length;
+    while(i--){
+      if(ss[i].src.indexOf('/os-load.js')>-1){
+        script=ss[i];
+        break;
+      }
     }
   }
+
+  var path = script.src;
+
+  var locateFile = function(url) {
+    if (url.indexOf('/') === -1) {
+      return path.replace(/(^.*)\/.*?$/, '$1/' + url);
+    }
+
+    return url;
+  };
+
+  var Module = {
+    'locateFile': locateFile,
+    'wasmBinaryFile': locateFile('os-wasm.wasm')
+  };
+
+  // fetch skips credentials by default, we want our wasm loader to use it
+  var origFetch = window.fetch;
+  var fetch = function(url, options) {
+    options = options || {};
+
+    if (!options.credentials) {
+      options.credentials = 'same-origin';
+    }
+
+    return origFetch(url, options);
+  };
 }
-
-var path = script.src;
-
-var locateFile = function(url) {
-  if (url.indexOf('/') === -1) {
-    return path.replace(/(^.*)\/.*?$/, '$1/' + url);
-  }
-
-  return url;
-};
-
-var Module = {
-  'locateFile': locateFile,
-  'wasmBinaryFile': locateFile('os-wasm.wasm')
-};
-
-// fetch skips credentials by default, we want our wasm loader to use it
-var origFetch = window.fetch;
-var fetch = function(url, options) {
-  options = options || {};
-
-  if (!options.credentials) {
-    options.credentials = 'same-origin';
-  }
-
-  return origFetch(url, options);
-};
